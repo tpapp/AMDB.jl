5ab4d1bcecc364dd423e525cc29ad9f7c14eca4f
Tamas K. Papp <tkpapp@gmail.com>
2017-05-15 09:56:46 +0200

    Refactored path handling, added tests.

--- src/AMDB_IHS.jl
+++ src/AMDB_IHS.jl
@@ -2,22 +2,55 @@ module AMDB_IHS
 
 export
     amdb_files_directory,
+    amdb_data_path,
     amdb_data_file,
     amdb_colnames
 
+"""
+    amdb_files_directory()
+
+Return the directoy for the AMDB data dump.
+
+The user should set the environment variable `AMDB_FILES`.
+"""
 function amdb_files_directory()
     key = "AMDB_FILES"
     @assert haskey(ENV, key) "You should put the path to the raw files in ENV[$key]."
     normpath(expanduser(ENV[key]))
 end
 
+"""
+    amdb_data_path(components...)
+
+Add `components` (directories, and potentially a filename) to the AMDB
+data dump directory.
+"""
+amdb_data_path(components...) = joinpath(amdb_files_directory(), components...)
+
+"""
+    amdb_data_file(year)
+
+The path for the AMDB data dump file (gzip-compressed CSV) for a given
+year. Example:
+
+```julia
+amdb_data_file(2000)
+```
+"""
 function amdb_data_file(year)
     @assert year ∈ 2000:2016 "Year outside the range provided."
-    joinpath(amdb_files_directory(), @sprintf("mon_ew_xt_uni_bus_%02d.csv.gz", year-2000))
+    ## special-case two years combined
+    yearnum = year ≤ 2014 ? @sprintf("%02d", year-2000) : "1516"
+    amdb_data_path("mon_ew_xt_uni_bus_$(yearnum).csv.gz")
 end
 
+"""
+    amdb_colnames()
+
+Return the AMDB column names for the data dump as a vector.
+"""
 function amdb_colnames()
-    header = readlines(joinpath(amdb_files_directory(), "mon_ew_xt_uni_bus.cols.txt"))[1]
+    header = readlines(amdb_data_path("mon_ew_xt_uni_bus.cols.txt"))[1]
     split(header, ';')
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,17 @@ using AMDB_IHS
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@testset "paths" begin
+    mktempdir() do dir
+        withenv("AMDB_FILES" => dir) do
+            @test amdb_files_directory() == dir
+            @test amdb_data_path("foo") == joinpath(dir, "foo")
+            @test amdb_data_file(2000) ==
+                joinpath(dir, "mon_ew_xt_uni_bus_00.csv.gz")
+            open(joinpath(dir, "mon_ew_xt_uni_bus.cols.txt"), "w") do io
+                write(io, "A;B;C\n")
+            end
+            @test amdb_colnames() == ["A","B","C"]
+        end
+    end
+end
