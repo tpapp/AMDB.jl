fb2c3cb31e73b505d44823453313a1e03ca8e114
Tamas K. Papp <tkpapp@gmail.com>
2017-09-26 18:32:12 +0200

    Simplified parsing code.
    Got rid of Nullables.

--- src/parsing.jl
+++ src/parsing.jl
@@ -1,46 +1,61 @@
+######################################################################
+# parsing primitives
+######################################################################
+
 # separator between fields
 const SEP = UInt8(';')
 
-eol_error() = error("Reached end of line")
+"Invalid character."
+const INVALID = -1
+
+"End of line."
+const EOL = -2
+
+"Empty field."
+const EMPTY = -3
 
 """
-    Nullable(d), pos = tryparse_base10_tosep(str, start, [stop])
+    validpos(pos)
 
-Parse digits from `str` as an integer, until encountering `sep` or the position
-`stop`.
-
-Return the parsed integer as a `Nullable`, and the position of the *next*
-unparsed character.
-
-When encountering a character that is not a digit or `SEP`, return a null value.
+Test if the returned position is valid. When not, this is an error and should be
+propagated.
 """
-function tryparse_base10_tosep(str::Vector{UInt8}, start, len = length(str))
+validpos(pos) = pos > 0
+
+"""
+    d, pos = tryparse_base10_tosep(str, start, [len])
+
+Parse digits from `str` as an integer, until encountering `sep` or EOL.
+
+Return the parsed integer and the position of the separator (or an error code,
+see `validpos`).
+"""
+function parse_base10_tosep(str::Vector{UInt8}, start, len = length(str))
     n = 0
     z = UInt8('0')
     pos = start
     @inbounds while pos ≤ len
         chr = str[pos]
-        chr == SEP && return Nullable(n, pos ≠ start), pos
+        chr == SEP && return n, pos == start ? EMPTY : pos
         maybe_digit = chr - z
         if 0 ≤ maybe_digit ≤ 9
             n = n*10 + maybe_digit
             pos += 1
         else
-            return Nullable(n, false), pos
+            return n, INVALID
         end
     end
-    eol_error()
+    n, EOL
 end
 
 """
-    Nullable(d) = tryparse_base10_fixed(str, start, [stop])
+    d, pos = parse_base10_fixed(str, start, stop)
 
 Parse digits from `str` between positions `start` and `stop` (inclusive) as an
-integer.
-
-Return null when anything other than digits are encountered.
+integer. `pos` is the position after parsing, it is either `stop + 1` or an
+error code.
 """
-function tryparse_base10_fixed(str::Vector{UInt8}, start, stop)
+function parse_base10_fixed(str::Vector{UInt8}, start, stop)
     n = 0
     z = UInt8('0')
     pos = start
@@ -50,64 +65,62 @@ function tryparse_base10_fixed(str::Vector{UInt8}, start, stop)
             n = n*10 + maybe_digit
             pos += 1
         else
-            return Nullable(n, false)
+            return n, INVALID
         end
     end
-    Nullable(n)
+    n, pos
 end
 
 """
-    tryparse_yyyymmdd(str)
+    d, pos = parse_date(str, start)
 
-
+Parse dates of the form "yyyymmdd". When the month or the day is zero, they are
+replaced by 1 (a peculiarity of the dataset).
 """
-function tryparse_date(str::Vector{UInt8}, start)
+function parse_date(str::Vector{UInt8}, start)
     stop = start+8
-    length(str) ≥ stop || eol_error()
+    length(str) ≥ stop || return Date(0), EOL
 
-    maybe_y = tryparse_base10_fixed(str, start, start+3)
-    isnull(maybe_y) && @goto invalid
+    y, pos = parse_base10_fixed(str, start, start+3)
+    validpos(pos) || return Date(0), pos
 
-    maybe_m = tryparse_base10_fixed(str, start+4, start+5)
-    isnull(maybe_m) && @goto invalid
+    m, pos = parse_base10_fixed(str, pos, pos+1)
+    validpos(pos) || return Date(0), pos
 
-    maybe_d = tryparse_base10_fixed(str, start+6, start+7)
-    isnull(maybe_d) && @goto invalid
+    d, pos = parse_base10_fixed(str, pos, pos+1)
+    validpos(pos) || return Date(0), pos
 
-    # FIXME terminator is hardcoded
-    str[stop] == SEP || @goto invalid # unterminated
+    str[pos] == SEP || return Date(0), INVALID
 
-    y = get(maybe_y)
-    m = max(get(maybe_m), 1)
-    m ≤ 12 || @goto invalid
-    d = max(get(maybe_d), 1)
-    d ≤ Base.Dates.daysinmonth(y, m) || @goto invalid
+    m = max(m, 1)
+    d = max(d, 1)
+    (m ≤ 12 && d ≤ Base.Dates.daysinmonth(y, m)) || return Date(0), INVALID
 
-    return Nullable(Date(y, m, d)), stop
-
-    @label invalid
-    return Nullable{Date}(), stop
+    Date(y, m, d), pos
 end
 
-function tryparse_skip(str::Vector{UInt8}, start, len = length(str))
+function parse_skip(str::Vector{UInt8}, start, len = length(str))
     pos = start
     @inbounds while pos ≤ len
         str[pos] == SEP && return pos
         pos += 1
     end
-    eol_error()
+    EOL
 end
 
-function tryparse_gobble(str::Vector{UInt8}, start, len = length(str))
-    stop = tryparse_skip(str, start, len)
-    @view(str[start:(stop-1)]), stop
+function parse_gobble(str::Vector{UInt8}, start, len = length(str))
+    pos = parse_skip(str, start, len)
+    @view(str[start:(pos-1)]), pos
 end
 
-function parsefield end
+"""
+    accumulate_field(
+"""
+function accumulate_field end
 
 struct SkipField end
 
-parsefield(::SkipField, str, start) = true, tryparse_skip(str, start)
+accumulate_field(::SkipField, str, start) = parse_skip(str, start)
 
 struct AccumulateField{T}
     values::Set{T}
@@ -117,36 +130,36 @@ AccumulateField(T) = AccumulateField(Set{T}())
 
 Base.values(acc::AccumulateField) = acc.values
 
-function parsefield(acc::AccumulateField{Int}, str, start)
-    value, pos = tryparse_base10_tosep(str, start)
-    !isnull(value) && push!(acc.values, get(value))
-    !isnull(value), pos
+function accumulate_field(acc::AccumulateField{Int}, str, start)
+    value, pos = parse_base10_tosep(str, start)
+    validpos(pos) && push!(acc.values, value)
+    pos
 end
 
-function parsefield(acc::AccumulateField{Vector{UInt8}}, str, start)
-    value, pos = tryparse_gobble(str, start)
-    value ∈ acc.values || push!(acc.values, value)
-    true, pos
+function accumulate_field(acc::AccumulateField{Vector{UInt8}}, str, start)
+    value, pos = parse_gobble(str, start)
+    validpos(pos) && push!(acc.values, value)
+    pos
 end
 
-function parsefield(acc::AccumulateField{Date}, str, start)
-    value, pos = tryparse_date(str, start)
-    !isnull(value) && push!(acc.values, get(value))
-    !isnull(value), pos
+function accumulate_field(acc::AccumulateField{Date}, str, pos)
+    value, pos = parse_date(str, pos)
+    validpos(pos) && push!(acc.values, value)
+    pos
 end
 
-parseline_(str, start) = nothing
+accumulate_line_(str, pos) = nothing
 
-parseline_(str, start, fieldparser) = parsefield(fieldparser, str, start)
+accumulate_line_(str, pos, fieldparser) = accumulate_field(fieldparser, str, pos)
 
-function parseline_(str, start, fieldparser, fieldparsers...)
-    status, start = parsefield(fieldparser, str, start)
-    if status
-        parseline_(str, start, fieldparsers...)
+function accumulate_line_(str, pos, fieldparser, fieldparsers...)
+    status, pos = accumulate_field(fieldparser, str, pos)
+    if validpos(pos)
+        accumulate_line_(str, pos, fieldparsers...)
     else
         # report error
     end
 end
 
 
-parseline(str, fieldparsers) = parseline_(str, 1, fieldparsers...)
+accumulate_line(str, fieldparsers) = accumulate_line_(str, 1, fieldparsers...)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,15 @@ using AMDB
 using Base.Test
 
 import AMDB:
-    tryparse_base10_tosep,
-    tryparse_base10_fixed,
-    tryparse_date,
-    tryparse_skip,
-    tryparse_gobble,
-    parsefield,
+    EMPTY, EOL, INVALID,
+    parse_base10_tosep,
+    parse_base10_fixed,
+    parse_date,
+    parse_skip,
+    parse_gobble,
+    accumulate_field,
     SkipField,
     AccumulateField
-
-≅ = isequal
 
 # made-up, not from the real dataset
 const sampleline = b"9997;19800101;19900101;0;0;AA;BB;"
@@ -34,39 +33,39 @@ end
 
 @testset "parsing integers" begin
     s = b"1970;1980;"
-    @test tryparse_base10_tosep(s, 1) ≅ (Nullable(1970), 5)
-    @test tryparse_base10_tosep(s, 6) ≅ (Nullable(1980), 10)
-    @test tryparse_base10_tosep(b";;", 2) ≅ (Nullable{Int}(), 2)
-    @test tryparse_base10_tosep(b";x;", 2) ≅ (Nullable{Int}(), 2)
-    @test tryparse_base10_fixed(s, 1, 4) ≅ Nullable(1970)
-    @test tryparse_base10_fixed(s, 6, 9) ≅ Nullable(1980)
-    @test tryparse_base10_fixed(b"19x0", 1, 4) ≅ Nullable{Int}()
+    @test parse_base10_tosep(s, 1) == (1970, 5)
+    @test parse_base10_tosep(s, 6) == (1980, 10)
+    @test parse_base10_tosep(b";;", 2)[2] == EMPTY
+    @test parse_base10_tosep(b";x;", 2)[2] == INVALID
+    @test parse_base10_fixed(s, 1, 4) == (1970, 5)
+    @test parse_base10_fixed(s, 6, 9) == (1980, 10)
+    @test parse_base10_fixed(b"19x0", 1, 4)[2] == INVALID
 end
 
 @testset "parsing dates" begin
-    @test tryparse_date(b"19800101;", 1) ≅ (Nullable(Date(1980, 1, 1)), 9)
-    @test tryparse_date(b"19800000;", 1) ≅ (Nullable(Date(1980, 1, 1)), 9)
-    @test tryparse_date(b"19809901;", 1) ≅ (Nullable{Date}(), 9)
+    @test parse_date(b"xx;19800101;", 4) == (Date(1980, 1, 1), 12)
+    @test parse_date(b"xx;19800000;", 4) == (Date(1980, 1, 1), 12)
+    @test parse_date(b"xx;19809901;", 4)[2] == INVALID
 end
 
 @testset "parsing or skipping strings" begin
-    @test tryparse_skip(b"12;1234;", 4) ≅ 8
-    @test tryparse_gobble(b"12;1234;", 4) ≅ (b"1234", 8)
+    @test parse_skip(b"12;1234;", 4) == 8
+    @test parse_gobble(b"12;1234;", 4) == (b"1234", 8)
 end
 
 @testset "field parsers" begin
-    @test parsefield(SkipField(), b"1980;", 1) == (true, 5)
-    @test parsefield(SkipField(), b"1980;1990;", 6) == (true, 10)
+    @test accumulate_field(SkipField(), b"1980;", 1) == 5
+    @test accumulate_field(SkipField(), b"1980;1990;", 6) == 10
     acc = AccumulateField(Int64)
-    @test parsefield(acc, b"1980;", 1) == (true, 5)
-    @test parsefield(acc, b"1980;1990;", 6) == (true, 10)
-    @test_throws ErrorException parsefield(acc, b"1980;1990", 6) # EOL
-    @test parsefield(acc, b";", 1)[1] == false
+    @test accumulate_field(acc, b"1980;", 1) == 5
+    @test accumulate_field(acc, b"1980;1990;", 6) == 10
+    @test accumulate_field(acc, b"1980;1990", 6) == EOL
+    @test accumulate_field(acc, b";", 1) == EMPTY
     @test sort(collect(values(acc))) == [1980, 1990]
     acc = AccumulateField(Date)
-    @test parsefield(acc, b"19800101;", 1) == (true, 9)
-    @test parsefield(acc, b"19800101;19900202;", 10) == (true, 18)
-    @test_throws ErrorException parsefield(acc, b"19800101", 1) # EOL
-    @test_throws ErrorException parsefield(acc, b";", 1)[1] == false
+    @test accumulate_field(acc, b"19800101;", 1) == 9
+    @test accumulate_field(acc, b"19800101;19900202;", 10) == 18
+    @test accumulate_field(acc, b"19800101", 1) == EOL
+    @test accumulate_field(acc, b";", 1) == EOL
     @test sort(collect(values(acc))) == [Date(1980, 1, 1), Date(1990, 2, 2)]
 end
