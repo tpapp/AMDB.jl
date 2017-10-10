f52e684916d9909795bf95ca11a51e0d870fe928
Tamas K. Papp <tkpapp@gmail.com>
2017-10-10 16:52:09 +0200

    Put file processing in the library.

--- script/first_pass.jl
+++ script/first_pass.jl
@@ -1,69 +1,58 @@
 using AMDB
 using ByteParsers
 using JLD
-using CodecZlib
-using WallTimeProgress
+using Parameters
 
 import AMDB:
     data_file, all_data_files, data_path, # data paths
-    narrowest_Int, to_narrowest_Int,      # narrowing integers
-    FileErrors, log_error                 # error logging
+    process_file,                         # file processing with parser
+    narrowest_Int, to_narrowest_Int       # narrowing integers
 
 ######################################################################
 # read whole file -- first pass
 ######################################################################
 
-function accumulate_stream(io::IO, parser_id_am, individuals, ams, errors::FileErrors;
-                           tracker_period = 10_000_000, max_lines = -1)
-    tracker = WallTimeTracker(tracker_period; item_name = "line")
-    while !eof(io) && (max_lines < 0 || count(tracker) < max_lines)
-        line_content = readuntil(io, 0x0a)
-        record = parsenext(parser_id_am, line_content, 1, UInt8(';'))
-        if isparsed(record)
-            id, am = unsafe_get(record)
-            individuals[id] = get(individuals, id, 0) + 1 # individual Individuals
-            push!(ams, am)                # AM status
-        else
-            log_error(errors, count(tracker), line_content, getpos(record))
-        end
-        increment!(tracker)
-    end
-end
-
-function accumulate_file(filename, parser_id_am, individuals, ams; args...)
-    io = GzipDecompressionStream(open(filename))
-    errors = FileErrors(filename)
-    accumulate_stream(io, parser_id_am, individuals, ams, errors; args...)
-    errors
-end
-
-# if this part breaks, then colnames changes, rewrite
+# if this part breaks, then column names changed, *rewrite*
 cols = AMDB.data_colnames()
 @assert cols[1] == "PENR"
 @assert cols[6] == "AM"
+
+struct FirstPass{T1,T2}
+    individuals::Dict{T1, Int}  # counts of individuals
+    AMs::Set{T2}                # labor market spells ("Arbeitsmarkt")
+end
+
+function (f::FirstPass)(record)
+    id, AM = record
+    @unpack individuals, AMs = f
+    individuals[id] = get(individuals, id, 0) + 1 # individual Individuals
+    push!(AMs, AM)                                # AM status
+end
+
+# convenience constructor
+FirstPass(parser::Line) =
+    FirstPass(Dict{parsedtype(parser[1]),Int}(), Set{parsedtype(parser[2])}())
 
 parser_id_am = Line(PositiveInteger(Int64),
                     Skip(), Skip(), Skip(), Skip(),
                     ViewBytes())
 
-individuals = Dict{parsedtype(parser_id_am[1]),Int}()
-
-ams = Set{parsedtype(parser_id_am[2])}()
+fp = FirstPass(parser_id_am)
 
 error_io = open(data_path("first_pass_errors.txt"), "w")
 println(error_io, "first pass started at $(Dates.now())")
 for file in all_data_files()
     println("processing $file")
-    err = accumulate_file(file, parser_id_am, individuals, ams)
+    err = process_file(file, parser_id_am, fp)
     show(error_io, err)
 end
 close(error_io)
 
-ids = to_narrowest_Int(collect(keys(individuals)))
-counts = collect(values(individuals))
+ids = to_narrowest_Int(collect(keys(fp.individuals)))
+counts = collect(values(fp.individuals))
 total_count = sum(counts)
-Tcounts = narrowest_Int(0, total_count)         # narrower type (need to do arithmetic)
-AM_keys = sort(copy.(collect(ams)), lt=lexless) # copy to avoid shared structure
+Tcounts = narrowest_Int(0, total_count) # narrower type (need to do arithmetic)
+AM_keys = sort(copy.(collect(fp.AMs)), lt=lexless) # copy to avoid shared structure
 
 save(data_path("first_pass.jld"),
      "ids", ids,
