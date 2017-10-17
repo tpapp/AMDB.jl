c4711ad752f9dea2e319b795090332b29a4b04d4
Tamas K. Papp <tkpapp@gmail.com>
2017-10-17 10:53:25 +0200

    Using RaggedData, mmap in first pass.

--- script/first_pass.jl
+++ script/first_pass.jl
@@ -1,11 +1,13 @@
 using AMDB
 using ByteParsers
 using JLD
-using Parameters
+using RaggedData
 
 import AMDB:
     data_file, all_data_files, data_path, # data paths
     process_file,                         # file processing with parser
+    AutoIndex,
+    first_pass_streams,
     narrowest_Int, to_narrowest_Int       # narrowing integers
 
 ######################################################################
@@ -17,44 +19,68 @@ cols = AMDB.data_colnames()
 @assert cols[1] == "PENR"
 @assert cols[6] == "AM"
 
-struct FirstPass{T1,T2}
-    individuals::Dict{T1, Int}  # counts of individuals
-    AMs::Set{T2}                # labor market spells ("Arbeitsmarkt")
-end
-
-function (f::FirstPass)(record)
-    id, AM = record
-    @unpack individuals, AMs = f
-    individuals[id] = get(individuals, id, 0) + 1 # individual Individuals
-    push!(AMs, AM)                                # AM status
+mutable struct FirstPass{Tid, TAM, TAM_ix, TIO}
+    id_counter::RaggedCounter{Tid, Int32}
+    AMs::AutoIndex{TAM, TAM_ix} # labor market spells ("Arbeitsmarkt")
+    stream_id::TIO
+    stream_AM_ix::TIO
+    stream_date_start::TIO
+    stream_date_stop::TIO
 end
 
 # convenience constructor
-FirstPass(parser::Line) =
-    FirstPass(Dict{parsedtype(parser[1]),Int}(), Set{parsedtype(parser[2])}())
+function FirstPass(parser::Line, base_filename)
+    FirstPass(RaggedCounter(Int32, Int32),
+              AutoIndex{Vector{UInt8}, Int8}(),
+              first_pass_streams(base_filename, "w")...)
+end
+
+# dumping dates to files
+Base.write(io::IO, date::Date) = unsafe_write(io, Ref(date), sizeof(Date))
+
+function Base.write(io::IO, x::T) where T
+    @assert isbits(T) "Can only write bits types."
+    unsafe_write(io, Ref(x), sizeof(T))
+end
+
+function (fp::FirstPass{Tid, TAM, TAM_ix, TIO})(record) where {Tid,TAM,TAM_ix,TIO}
+    id_wide, date_start, date_stop, AM = record
+    id = Tid(id_wide)           # conversion to (possibly) narrower type
+    push!(fp.id_counter, id)    # record counter
+    # save into streams
+    write(fp.stream_id, id)
+    write(fp.stream_AM_ix, TAM_ix(fp.AMs[AM])) # autoindexing new values
+    write(fp.stream_date_start, date_start)
+    write(fp.stream_date_stop, date_stop)
+end
+
+function Base.close(fp::FirstPass)
+    close(fp.stream_id)
+    close(fp.stream_AM_ix)
+    close(fp.stream_date_start)
+    close(fp.stream_date_stop)
+end
 
 parser_id_am = Line(PositiveInteger(Int64),
-                    Skip(), Skip(), Skip(), Skip(),
+                    DateYYYYMMDD(), DateYYYYMMDD(), Skip(), Skip(),
                     ViewBytes())
 
-fp = FirstPass(parser_id_am)
-
+fp = FirstPass(parser_id_am, "first_pass")
 error_io = open(data_path("first_pass_errors.txt"), "w")
-println(error_io, "first pass started at $(Dates.now())")
+println(error_io, "first pass started at $(Dates.now()) on machine $(gethostname())")
 for file in all_data_files()
     println("processing $file")
     err = process_file(file, parser_id_am, fp)
     show(error_io, err)
 end
 close(error_io)
+close(fp)
 
-ids = to_narrowest_Int(collect(keys(fp.individuals)))
-counts = collect(values(fp.individuals))
-total_count = sum(counts)
-Tcounts = narrowest_Int(0, total_count) # narrower type (need to do arithmetic)
-AM_keys = sort(copy.(collect(fp.AMs)), lt=lexless) # copy to avoid shared structure
+save(data_path("first_pass_meta.jld"),
+     "id_counter", fp.id_counter,
+     "AM_keys", keys(fp.AMs))
 
-save(data_path("first_pass.jld"),
-     "ids", ids,
-     "counts", Tcounts.(counts),
-     "AM_keys", AM_keys)
+## run this for testing
+fp_meta = load(data_path("first_pass_meta.jld"))
+@assert isequal(fp.id_counter.dict, fp_meta["id_counter"].dict)
+@assert isequal(fp_meta["AM_keys"], keys(fp.AMs))
--- src/mmap_access.jl
+++ src/mmap_access.jl
@@ -32,6 +32,22 @@ Base.size(csw::CumSumWrapper) = (length(csw), )
 
 meta_records_file(base_filename) = data_path(base, "_meta.jld")
 
+function first_pass_filenames(base_filename)
+    fn(part) = data_path("$(base_filename)_$(part).bin")
+    fn("id"), fn("AM_ix"), fn("date_start"), fn("date_stop")
+end
+
+function first_pass_streams(base_filename, mode)
+    map(fn -> open(fn, mode), first_pass_filenames(base_filename))
+end
+
+function first_pass_mmap(base_filename, N, mode = "r+")
+    id, AM_ix, date_start, date_stop = first_pass_streams(base_filename, mode)
+    mmapped_(stream, T) = Mmap.mmap(stream, Vector{T}, (N,))
+    (mmapped_(id, Int32), mmapped_(AM_ix, Int8),
+     mmapped_(date_start, Date), mmapped_(date_stop, Date))
+end
+
 function mmapped_arrays(base_filename, N, mode = "r")
     filename_(part) = data_path(base_filename, "_", part)
     mmapped_(part, T) = Mmmap.mmap(open(filename_(part), mode), Array{T}, (N,))
@@ -44,17 +60,14 @@ end
 
 
 """
-    
+    cumsum2counter(cumsums)
+
+Return an initialized counter from cumulative sums. When adding an element, it
+should be incremented, _then_ the incremented value returned.
 """
 function cumsum2counter(cumsums::Vector{T}) where T
     counters = similar(cumsums)
-    counters[1] = 1
-    for i in 2:length(cumsums)
-        counters[i] = cumsums[i-1] + one(T)
-    end
+    counters[1] = 0
+    counters[2:end] .= cumsums[1:(end-1)]
     counters
-end
-
-function parse_and_place_AM_spells(io, errors, counters)
-    
 end
