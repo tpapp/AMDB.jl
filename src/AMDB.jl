7dad0b7e5a948962a602375f7462c8b4d01fd266
Tamas K. Papp <tkpapp@gmail.com>
2017-10-19 19:50:30 +0200

    Added date handling.

--- src/AMDB.jl
+++ src/AMDB.jl
@@ -5,6 +5,7 @@ using DocStringExtensions
 include("paths.jl")
 include("errors.jl")
 include("autoindex.jl")
+include("dates.jl")
 include("process.jl")
 include("utilities.jl")
 include("mmap_access.jl")
