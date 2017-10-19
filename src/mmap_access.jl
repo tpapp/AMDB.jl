729f6aad8b86295dea738aeeed49145f4bf5e8db
Tamas K. Papp <tkpapp@gmail.com>
2017-10-19 19:43:59 +0200

    Cleanup of file.
    Indexing code migrated to RaggedData.

--- src/mmap_access.jl
+++ src/mmap_access.jl
@@ -1,35 +1,5 @@
 using JLD
 
-"""
-    cumsum2range(cumsums, i)
-
-Let `c` be a vector of (integer) counts, and `cumsums` its cumulative sum.
-
-For an index `i`, return a `UnitRange` that corresponds to integers between
-`c₁+...+cᵢ+1` and `c₁+...+cᵢ₊₁`, inclusive.
-"""
-function cumsum2range(cumsums::Vector{T}, i) where {T <: Integer}
-    UnitRange(i == 1 ? one(T) : cumsums[i-1]+one(T), cumsums[i])
-end
-
-"""
-    CumSumWrapper(cumsums)
-
-A wrapper for `cumsum2range`, returning ranges for each index — representing a
-vector, calculating elements on demand.
-"""
-struct CumSumWrapper{T}
-    cumsums::Vector{T}
-end
-
-Base.length(csw::CumSumWrapper) = length(csw.cumsums)
-
-Base.getindex(csw::CumSumWrapper, i) = cumsum2range(csw.cumsums, i)
-
-Base.eltype(csw::CumSumWrapper{T}) where T = UnitRange{T}
-
-Base.size(csw::CumSumWrapper) = (length(csw), )
-
 meta_records_file(base_filename) = data_path(base, "_meta.jld")
 
 function first_pass_filenames(base_filename)
@@ -41,12 +11,25 @@ function first_pass_streams(base_filename, mode)
     map(fn -> open(fn, mode), first_pass_filenames(base_filename))
 end
 
-function first_pass_mmap(base_filename, N, mode = "r+")
-    id, AM_ix, date_start, date_stop = first_pass_streams(base_filename, mode)
-    mmapped_(stream, T) = Mmap.mmap(stream, Vector{T}, (N,))
-    (mmapped_(id, Int32), mmapped_(AM_ix, Int8),
-     mmapped_(date_start, Date), mmapped_(date_stop, Date))
+# function first_pass_mmap(base_filename, N, mode = "r+")
+#     id, AM_ix, date_start, date_stop = first_pass_streams(base_filename, mode)
+#     mmapped_(stream, T) = Mmap.mmap(stream, Vector{T}, (N,))
+#     (mmapped_(id, Int32), mmapped_(AM_ix, Int8),
+#      mmapped_(date_start, Date), mmapped_(date_stop, Date))
+# end
+
+
+function mmapped_vector(filename, T::Type, len::Int, mode = "r+")
+    stream = open(data_path(filename), mode)
+    Mmap.mmap(stream, Vector{T}, (len,))
 end
+
+
+
+# function second_pass_mmap(base_filename, N, mode = "r+")
+#     function mmapped_(part, T)
+#         Mmap.mmap(open(data_path("$(base_filename)_$(part).bin")))
+# end
 
 function mmapped_arrays(base_filename, N, mode = "r")
     filename_(part) = data_path(base_filename, "_", part)
@@ -56,18 +39,4 @@ function mmapped_arrays(base_filename, N, mode = "r")
      mmapped_("spell_start", Date),
      mmapped_("spell_stop", Date),
      mmapped_("spell_raw_type", Date))
-end
-
-
-"""
-    cumsum2counter(cumsums)
-
-Return an initialized counter from cumulative sums. When adding an element, it
-should be incremented, _then_ the incremented value returned.
-"""
-function cumsum2counter(cumsums::Vector{T}) where T
-    counters = similar(cumsums)
-    counters[1] = 0
-    counters[2:end] .= cumsums[1:(end-1)]
-    counters
 end
