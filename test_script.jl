5ace1ee0ab11ba7917169fc61e494c4e66ab8d2a
Tamas K. Papp <tkpapp@gmail.com>
2017-12-05 11:25:59 +0100

    Cleanup of testing framework.

---
+++ test_script.jl
@@ -0,0 +1,11 @@
+Pkg.clone(pwd())
+# install unregistered packages
+Pkg.clone("https://github.com/tpapp/RaggedData.jl.git")
+Pkg.clone("https://github.com/tpapp/ByteParsers.jl.git")
+Pkg.clone("https://github.com/tpapp/DiscreteRanges.jl.git")
+Pkg.clone("https://github.com/tpapp/LargeColumns.jl.git")
+Pkg.clone("https://github.com/tpapp/FlexDates.jl.git")
+Pkg.clone("https://github.com/tpapp/WallTimeProgress.jl.git")
+# build and test
+Pkg.build("AMDB")
+Pkg.test("AMDB"; coverage=true)
