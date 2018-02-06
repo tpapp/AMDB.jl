9a69cf03b691d65915fe1a3e2a0c2716a75fe5f4
Tamas K. Papp <tkpapp@gmail.com>
2018-02-06 13:36:12 +0100

    Example for counting duplicates.

---
+++ script/investigate_duplicates.jl
@@ -0,0 +1,45 @@
+using AMDB
+using DocStringExtensions
+using Distributions
+using DataStructures
+using ProgressMeter
+
+
+# data part
+
+# read the data
+data = AMDB.collated_dataset("collated");
+
+function count_duplicates(data, individual_index)
+    stichtag = individual_columns(data, individual_index, [:STICHTAG])[1]
+    mis = get_mis.(stichtag)
+    length(mis) - length(unique(mis))
+end
+
+function individuals_with_duplicates(data)
+    duplicates = Vector{Tuple{Int, Int}}() # index, how many
+    @showprogress for i in indices(data.ix, 1)
+        d = count_duplicates(data, i)
+        if d > 0
+            push!(duplicates, (i, d))
+        end
+    end
+    duplicates
+end
+
+function select_duplicates(v)
+    c = counter(v)
+    map(elt -> c[elt] > 1, v)
+end
+
+function duplicate_observations(data, individual_index)
+    df = individual_df(data, individual_index)
+    dup = select_duplicates(df[:STICHTAG])
+    df[dup, :]
+end
+
+count_duplicates(data, 400)
+
+dd = duplicate_observations(data, 400)
+
+showall(dd)
